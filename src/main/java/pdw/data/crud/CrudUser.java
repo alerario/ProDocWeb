1de1d44480aedc109563cc3aa88c9f5bb472e39e
alexandrelerario <alexandrelerario@192.168.0.110>
2021-03-07 20:04:33 -0300

    Criado mecanismo de autenticacao com md5.

--- src/main/java/pdw/data/crud/CrudUser.java
+++ src/main/java/pdw/data/crud/CrudUser.java
@@ -5,8 +5,13 @@
  */
 package pdw.data.crud;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.xml.bind.DatatypeConverter;
+import pdw.data.model.User;
 
 /**
  *
@@ -20,7 +25,6 @@ public class CrudUser extends AbstractCrud<pdw.data.model.User> {
         super(pdw.data.model.User.class);
     }
 
-  
     @Override
     protected EntityManager getEntityManager() {
         if (em == null) {
@@ -28,5 +32,49 @@ public class CrudUser extends AbstractCrud<pdw.data.model.User> {
         }
         return em;
     }
+
+    // os metodos abaixo sao opcionais
+    public User getAuth(String user_email, String password) {
+        try {
+            User user = (User) getEntityManager().createNamedQuery("User.findByEmail").setParameter("email", user_email).getSingleResult();
+            String md5pass = getMd5(password);
+            if (user.getPassword().toUpperCase().equals(md5pass)) {
+                return user;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private String getMd5(String pass) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(pass.getBytes());
+            byte[] digest = md.digest();
+            String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+            return myHash;
+// m.update(pass.getBytes(), 0, pass.length());
+            // return Arrays.toString(m.digest());
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+    /*
+     algumas chaves
+     abc123
+e99a18c428cb38d5f260853678922e03
+
+r2d2
+3e0fd1ad8efb39d90b8cd3b04a6c94f1
+
+Starwar5
+7b03c175eb89d1a2640e0ebef6ea68a9
+     
+     */
 
 }
---
+++ src/main/java/pwd/jsf/JsfAuth.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pwd.jsf;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import pdw.data.model.User;
+
+/**
+ *
+ * @author alexandrelerario
+ */
+@Named(value = "jsfAuth")
+@SessionScoped
+public class JsfAuth implements Serializable {
+
+    /**
+     * Creates a new instance of JsfPassaporte
+     */
+    //classe utilizada para efetuar a autenticacao e autorizacao
+    public JsfAuth() {
+    }
+
+    private User user;
+    private boolean auth = false;
+    private String login;
+    private String password;
+
+    public String authenticate() {
+        user = new pdw.data.crud.CrudUser().getAuth(login, password);
+        auth = user != null;
+        if (auth) {
+            return "mainmenu";
+        }
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login ou senha inválidos.", "Error"));
+        return null;
+    }
+
+    public String logout(){
+        auth=false;
+        user=null;
+        return "index";
+    }
+    
+    public boolean getAuth() {
+        return auth;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
