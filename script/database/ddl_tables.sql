80793c091431270c9120f0defb2589a2ab39e579
alerario <alerario@gmail.com>
2021-03-20 07:24:39 -0300

    Update ddl_tables.sql
    Ajuste do nome do campo de update para updated. Ajuste do tamanho do campo description de 200 para 500.

--- script/database/ddl_tables.sql
+++ script/database/ddl_tables.sql
@@ -28,12 +28,12 @@ CREATE TABLE public.sworganization
 CREATE TABLE public.swprocess (
 	id SERIAL NOT NULL,
 	name character varying(50),
-	description character varying(200),
+	description character varying(500),
 	images bytea,
 	modified DATE,
 	organization integer NOT NULL,
 	created timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	update TIMESTAMP NOT NULL,
+	updated TIMESTAMP NOT NULL,
 	CONSTRAINT swprocess_pk PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
